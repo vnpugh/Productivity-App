b5292c8cfb5906e3d7fb757849753b1d6e25f061
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2023-09-15 00:57:19 -0500

    creating user model

---
+++ src/main/java/com/example/todoapp/Models/Task.java
@@ -0,0 +1,4 @@
+package com.example.todoapp.Models;
+
+public class Task {
+}
---
+++ src/main/java/com/example/todoapp/Models/User.java
@@ -0,0 +1,63 @@
+package com.example.todoapp.Models;
+
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
